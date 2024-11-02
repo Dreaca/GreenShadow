51d92c44be47daf6b4a562c43f7fb987a16c99d5
dreaca <buddhikadreaca@gmail.com>
2024-11-02 09:54:01 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse/greenshadow/GreenShadowApplication.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.greenshadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowApplication.class, args);
+    }
+}
